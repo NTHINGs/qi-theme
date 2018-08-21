b1d939dc4bdfb07aff8b334463051eb2d9cc747f
Mauricio Martinez <n_othing@hotmail.com>
2018-08-21 13:10:23 -0500

    Blog template

--- page-templates/blog.php
+++ page-templates/blog.php
@@ -1,49 +1,76 @@
 <?php
 /**
- * The template used for displaying blog posts
+ * Theme Page Section for our theme.
  *
  * @package    ThemeGrill
  * @subpackage Accelerate
  * @since      Accelerate 1.0
  */
-?>
-<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-	<?php do_action( 'accelerate_before_post_content' ); ?>
+get_header(); ?>
 
-	<header class="entry-header">
-		<h2 class="entry-title">
-			<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
-		</h2>
-	</header>
+<?php do_action( 'accelerate_before_body_content' ); ?>
 
-	<?php
-	if ( 'post' == get_post_type() ) :
-		accelerate_entry_meta();
-	endif;
-	?>
+	<div id="primary">
+		<div id="content" class="clearfix">
+			<?php while ( have_posts() ) : the_post(); ?>
 
-	<?php
-	if ( has_post_thumbnail() ) {
-		$image = '';
-		$title_attribute = get_the_title( $post->ID );
-		$image .= '<figure class="post-featured-image">';
-		$image .= '<a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '">';
-		$image .= get_the_post_thumbnail( $post->ID, 'featured-blog-large', array(
-				'title' => esc_attr( $title_attribute ),
-				'alt'   => esc_attr( $title_attribute ),
-			) ) . '</a>';
-		$image .= '</figure>';
-		echo $image;
-	}
-	?>
+				<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+					<?php do_action( 'accelerate_before_post_content' ); ?>
 
-	<div class="entry-content clearfix">
-		<?php
-		global $more;
-		$more = 0;
-		the_content( '<span>' . __( 'Read more', 'accelerate' ) . '</span>' );
-		?>
-	</div>
+					<header class="entry-header">
+						<h2 class="entry-title">
+							<a href="<?php the_permalink(); ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+						</h2>
+					</header>
 
-	<?php do_action( 'accelerate_after_post_content' ); ?>
-</article>
+					<?php
+					if ( 'post' == get_post_type() ) :
+						accelerate_entry_meta();
+					endif;
+					?>
+
+					<?php
+					if ( has_post_thumbnail() ) {
+						$image = '';
+						$title_attribute = get_the_title( $post->ID );
+						$image .= '<figure class="post-featured-image">';
+						$image .= '<a href="' . get_permalink() . '" title="' . the_title_attribute( 'echo=0' ) . '">';
+						$image .= get_the_post_thumbnail( $post->ID, 'featured-blog-large', array(
+								'title' => esc_attr( $title_attribute ),
+								'alt'   => esc_attr( $title_attribute ),
+							) ) . '</a>';
+						$image .= '</figure>';
+						echo $image;
+					}
+					?>
+
+					<div class="entry-content clearfix">
+						<?php
+						global $more;
+						$more = 0;
+						the_content( '<span>' . __( 'Read more', 'accelerate' ) . '</span>' );
+						?>
+					</div>
+
+					<?php do_action( 'accelerate_after_post_content' ); ?>
+				</article>
+
+				<?php
+				do_action( 'accelerate_before_comments_template' );
+				// If comments are open or we have at least one comment, load up the comment template
+				if ( comments_open() || '0' != get_comments_number() ) {
+					comments_template();
+				}
+				do_action( 'accelerate_after_comments_template' );
+				?>
+
+			<?php endwhile; ?>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+<?php accelerate_sidebar_select(); ?>
+
+<?php do_action( 'accelerate_after_body_content' ); ?>
+
+<?php get_footer(); ?>
